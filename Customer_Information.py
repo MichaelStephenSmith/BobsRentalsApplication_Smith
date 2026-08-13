8ad5aeca00bfdb757d44c449784217d633ed0837
Michael Smith <mssmith5@cincinnatistate.edu>
2026-08-13 19:31:27 -0400

    Added Customer Information Class

---
+++ Customer_Information.py
@@ -0,0 +1,19 @@
+#-----------------------------------------------------
+# Class name: Customer_Information
+# Class definition: This class gathers the customers 
+# name and ID number.
+#-----------------------------------------------------
+
+class Customer_Information:
+
+        #-----------------------------------------------------
+        # Constructor/Initialization
+        #-----------------------------------------------------
+
+        def __init__(self, strName, intIDnumber):
+            self.strName = strName
+            self.intIDNumber = intIDnumber 
+
+
+
+
